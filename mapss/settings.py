4c9c1cd365b3486f0064cbb260d7dd373322a818
fahadcr14 <bsdsf21m014@pucit.edu.pk>
2023-07-12 22:28:56 +0500

    v15.2

--- mapss/settings.py
+++ mapss/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e!lf)ewtn4ew_044_m!dos%snzni_au**eo30q1(x(8lc1knjc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS=True
