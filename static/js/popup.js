3b805bbf0d0c2d7a56d609e9ac4d6795b9136326
fahadcr14 <bsdsf21m014@pucit.edu.pk>
2023-08-16 10:58:13 +0500

    popup

---
+++ static/js/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const backButton = document.getElementById("q2-options");
+
+    backButton.addEventListener("click", function() {
+        console.log('Popup is called')
+        //navigateToPreviousPage();
+    });
+});
